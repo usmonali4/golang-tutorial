7537b318ed440de9ffa8f2263f493d4481bc779b
Usmon <usmonpahlavonov@gmail.com>
2024-10-20 11:51:57 +0900

    init running server

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/usmonali4/rssagg
+
+go 1.22.5
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	// "fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main(){
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+}
