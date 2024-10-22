bd2aad41f327f12b921580815feacc237891a5e5
Usmon <usmonpahlavonov@gmail.com>
2024-10-22 11:00:30 +0900

    connected CRUD API with PostgresSQL db using gorm dep

--- second_lesson/cmd/main.go
+++ second_lesson/cmd/main.go
@@ -6,18 +6,22 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/usmonali4/gotour/pkg/db"
 	"github.com/usmonali4/gotour/pkg/handlers"
 )
 
 func main() {
+
+	DB := db.Init()
+	h := handlers.New(DB)
 	router := mux.NewRouter()
 
-	router.HandleFunc("/books", handlers.GetAllBooks).Methods(http.MethodGet)
-	router.HandleFunc("/books", handlers.AddBook).Methods(http.MethodPost)
-	router.HandleFunc("/books/{id}", handlers.GetBook).Methods(http.MethodGet)
-	router.HandleFunc("/books/{id}", handlers.UpdateBook).Methods(http.MethodPut)
-	router.HandleFunc("/books/{id}", handlers.DeleteBook).Methods(http.MethodDelete)
-	
+	router.HandleFunc("/books", h.GetAllBooks).Methods(http.MethodGet)
+	router.HandleFunc("/books", h.AddBook).Methods(http.MethodPost)
+	router.HandleFunc("/books/{id}", h.GetBook).Methods(http.MethodGet)
+	router.HandleFunc("/books/{id}", h.UpdateBook).Methods(http.MethodPut)
+	router.HandleFunc("/books/{id}", h.DeleteBook).Methods(http.MethodDelete)
+
 
 	log.Println("API is running")
 	http.ListenAndServe(":4000", router)
